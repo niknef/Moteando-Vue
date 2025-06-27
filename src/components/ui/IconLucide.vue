c57d757bf92cac03833a499d4b7e198d44cf4bd5
niknef <nicolas.firpo@davinci.edu.ar>
2025-06-27 13:30:21 -0300

    add icon

---
+++ src/components/ui/IconLucide.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { computed } from 'vue'
+import * as lucide from 'lucide-vue-next'
+const props = defineProps({ name: String, size: { type: [Number,String], default: 24 }, active: Boolean })
+const IconComp = computed(() => lucide[props.name])
+</script>
+
+<template>
+  <component :is="IconComp" :size="props.size" stroke-width="1.5"
+             :class="active ? 'text-orange-400' : 'text-gray-400'" />
+</template>
